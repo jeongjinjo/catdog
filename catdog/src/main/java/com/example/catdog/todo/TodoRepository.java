9bf05f1c615111f0a9730572852851f31868d26f
JjungEeunAae <wjddmsdo77@naver.com>
2024-03-23 15:39:32 +0900

    오류 수정 확인하여 다시 메인으로 병합

--- catdog/src/main/java/com/example/catdog/todo/TodoRepository.java
+++ catdog/src/main/java/com/example/catdog/todo/TodoRepository.java
@@ -11,17 +11,17 @@ import java.util.Optional;
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
     // 로그인 한 사람이 속한 그룹별 반려동물 의 할 일
-    @Query(value = "SELECT ct.group_num, d.pet_num, d.todo, d.start_date, d.start_id " +
-            "FROM Todo d " +
-            "JOIN Care_target ct ON ct.pet_num=d.pet_num " +
-            "WHERE ct.group_num IN ( " +
-            "   SELECT cg.group_key " +
-            "   FROM Care_group cg " +
-            "   JOIN Member m ON m.member_id=cg.member.member_id " +
-            "   WHERE m.member_id= :id " +
-            "   AND cg.resign_yn='N' " +
-            "   AND m.resign_yn='N' " +
-            ") "
-    )
-    public Optional<List<Object[]>> findByGroupPetTodo(@Param("id") String id);
+//    @Query(value = "SELECT ct.group_num, d.pet_num, d.todo, d.start_date, d.start_id " +
+//            "FROM Todo d " +
+//            "JOIN CareTarget ct ON ct.pet_num=d.pet_num " +
+//            "WHERE ct.group_num IN ( " +
+//            "   SELECT cg.group_key " +
+//            "   FROM CareGroup cg " +
+//            "   JOIN Member m ON m.member_id=cg.member.member_id " +
+//            "   WHERE m.member_id= :id " +
+//            "   AND cg.resign_yn='N' " +
+//            "   AND m.resign_yn='N' " +
+//            ") "
+//    )
+//    public Optional<List<Object[]>> findByGroupPetTodo(@Param("id") String id);
 }
