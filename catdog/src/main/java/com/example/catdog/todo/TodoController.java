d22e22623ea79d8bd9bba4454326ced2115a741e
yayoung92 <yangga0744@gmail.com>
2024-03-28 15:27:30 +0900

    코드 정리와 예외처리 중

--- catdog/src/main/java/com/example/catdog/todo/TodoController.java
+++ catdog/src/main/java/com/example/catdog/todo/TodoController.java
@@ -2,6 +2,7 @@ package com.example.catdog.todo;
 
 import com.example.catdog.enum_column.Comp_yn;
 import com.example.catdog.enum_column.Resign_yn;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +29,8 @@ public class TodoController {
      * @param todo : 할일
      * @return : 반려동물의 todo(할 일)을 등록한다.
      */
+    @Operation(summary = "반려동물의 할일 등록"
+            , description = "할일관리를 만들어주기 위한 INSERT 기능")
     @PostMapping()
     public Todo getPetTodo(@RequestBody Todo todo){
         Todo todos= Todo.builder()
@@ -43,20 +46,24 @@ public class TodoController {
     }
 
     /**
-     * @param id : 로그인 한 아이디
+     * @param member_id : 로그인 한 아이디
      * @param petNum : 선택한 반려동물
      * @param date : 날짜
      * @return : 선택한 동물과 날짜로 할 일 조회한다. 디폴트는 오늘날짜, 첫번째 반려동물
      */
+    @Operation(summary = "반려동물의 할일 조회"
+            , description = "반려동물의 고유번호로 할일 조회. SELECT 기능")
     @GetMapping()
-    public ResponseEntity<Map<Integer, List<Object>>> todolist(@RequestParam String id, Integer petNum, Date date){
-        Map<Integer, List<Object>> todolists = todoService.getdatePetTodoList(id, petNum, date);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(todolists);
+    public ResponseEntity<Map<Integer, List<Object>>> todolist(@RequestParam String member_id, Integer petNum, Date date){
+        Map<Integer, List<Object>> todolist = todoService.getdatePetTodoList(member_id, petNum, date);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(todolist);
     }
 
     /**
-     * @return : 로그인 한 사람이름이 본인이 작석한 오늘날짜의 할일 수정하기
+     * @return : 오늘날짜의 할일 수정하기
      */
+    @Operation(summary = "반려동물의 할일 수정"
+            , description = "반려동물 고유번호를 이용해 오늘 날짜일 경우 수정가능. UPDATE 기능")
     @PutMapping()
     public ResponseEntity<Todo> update(@RequestBody TodoDTO todoDTO) {
         ModelMapper mapper = new ModelMapper();
@@ -71,6 +78,8 @@ public class TodoController {
      * @param todo_num : 삭제할 반려동물 고유번호
      * @return : 반려동물의 오늘날짜의 할일 삭제하기
      */
+    @Operation(summary = "반려동물의 할일 삭제"
+            , description = "반려동물 고유번호 받아서 삭제 처리. 할일 완료 안 되 것만 삭제 가능. UPDATE 기능.")
     @PutMapping("{todo_num}")
     public ResponseEntity<Todo> delete(@PathVariable Integer todo_num) {
         Todo deleteTodo = todoService.deleteTodo(todo_num);
