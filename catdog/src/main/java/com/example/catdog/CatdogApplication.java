3b66660be986bb1128a9e6203cbb2d23c1bc526c
JjungEeunAae <wjddmsdo77@naver.com>
2024-03-28 12:56:55 +0900

    Tomcat 설정(서버구축) 및 테스트 완료

--- catdog/src/main/java/com/example/catdog/CatdogApplication.java
+++ catdog/src/main/java/com/example/catdog/CatdogApplication.java
@@ -2,12 +2,19 @@ package com.example.catdog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CatdogApplication {
+public class CatdogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatdogApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CatdogApplication.class);
 	}
 
 }
