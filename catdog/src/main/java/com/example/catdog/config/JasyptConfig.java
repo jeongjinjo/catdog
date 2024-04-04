97b18d2cd031e429439b59e6c05437e7a3ce404d
DESKTOP-AGV6E1U\GoChanWoo <wnf2255@gmail.com>
2024-04-04 14:27:06 +0900

    240404

--- catdog/src/main/java/com/example/catdog/config/JasyptConfig.java
+++ catdog/src/main/java/com/example/catdog/config/JasyptConfig.java
@@ -1,13 +1,17 @@
 package com.example.catdog.config;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableEncryptableProperties
 public class JasyptConfig {
 
-    /*@Value("${jasypt.encryptor.password}")
+    @Value("${jasypt.encryptor.password}")
     String password;
 
     @Bean
@@ -24,5 +28,5 @@ public class JasyptConfig {
         encryptor.setConfig(config);
         return encryptor;
 
-    }*/
+    }
 }
